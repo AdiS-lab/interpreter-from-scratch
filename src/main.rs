57d78fb49415b73e38afa0f3af70f3d598efe804
Adi Shankar <adishankar219@gmail.com>
2026-06-30 16:59:35 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,7 @@ fn main() {
 
             // TODO: Uncomment the code below to pass the first stage
             if !file_contents.is_empty() {
+                eprintln!("this is file contents {}" ,file_contents)
                 for ch in file_contents.chars(){
                     match ch {
                         '(' => println!("LEFT_PAREN ( null"),
