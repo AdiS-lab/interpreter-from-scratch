eb95c14819fb73d8b6eebb96f3382c806909f11e
Adi Shankar <adishankar219@gmail.com>
2026-06-30 17:05:28 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -31,8 +31,8 @@ fn main() {
                         ')' => println!("RIGHT_PAREN ) null"),
                         _ => {}
                     } 
-                    println!("EOF  null");
                 }
+                println!("EOF  null");
             } else {
                 println!("EOF  null"); // Placeholder, replace this line when implementing the scanner
             }
