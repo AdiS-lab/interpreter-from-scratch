4a7d30cb6fd5f5c2484d6ccfcaed62f2c58c38cb
Adi Shankar <adishankar219@gmail.com>
2026-07-12 12:27:27 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -88,6 +88,10 @@ fn main() -> ExitCode {
                         '!' => {
                             println!("made it into pipe");
                         }
+                        ' ' =>{
+                            while str_iter.next() != Some(' '){
+                            }
+                        }
                         _ => {
                             println!("this is char {}", ch);
                             err_exists = true;
