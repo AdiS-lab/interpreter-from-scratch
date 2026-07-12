4906654858e0175a1eed32c76243c73ac03941b1
Adi Shankar <adishankar219@gmail.com>
2026-07-12 13:34:18 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -66,7 +66,7 @@ fn main() -> ExitCode {
                                 literal.push(newCh);//abcd... 
                             }; // once reaching None, will have the strings. 
                             if !lexeme.ends_with('"'){
-                                errExists = true
+                                errExists = true;
                                 eprintln!("[line {}] Error: Error: Unterminated string.", newLine);
                             }else{
                                 println!("STRING {} {}", lexeme, literal);
