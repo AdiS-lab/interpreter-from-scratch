bb06ae4ed87a52ed3ccf09ac1dce8ea665c1dbb2
Adi Shankar <adishankar219@gmail.com>
2026-07-13 16:51:14 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -182,7 +182,7 @@ fn main() -> ExitCode {
         "parse" => { // iterator. 
             let tokenStr = tokenize(file_contents);  
             let tokens: Vec<String>= tokenStr.split(",").map(|s| s.to_string()).collect();
-            println!("{:?}", tokens[0].split(" ").map(|s| s.to_string()).collect()::<Vec<String>>[0]);
+            println!("{:?}", tokens[0].split(" ").map(|s| s.to_string()).collect::<Vec<String>>()[0]);
             return ExitCode::from(0)
         },
         _ => {
