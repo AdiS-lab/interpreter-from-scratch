900cafa21146d4c63229960ddf40ecf1e958076f
Adi Shankar <adishankar219@gmail.com>
2026-07-14 08:04:52 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -341,7 +341,7 @@ fn main() -> ExitCode {
             while let Some(token) = token_iter.next(){ // [NUMBER 50 50.0]
                 let ind_tokens: Vec<&str> = token.split(" ").collect();
                 let mut fullStr = String::new();
-                if let Some(tk_type) = ind_tokens.get(0){
+                if ind_tokens.get(0){ // <String>.split(space) => Vec<&str>, so get(0) = <&str>
                  match tk_type{
                     "NUMBER" => fullStr.push_str("{}", ind_tokens.get(2).unwrap_or("")),
                     "STRING" => fullStr.push_str("{}", token.split('"').map(|s| s.to_string()).collect::<Vec<String>>()[1]),
