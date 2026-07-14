20a6e4869d91615e8467607ea256df2c00da4b35
Adi Shankar <adishankar219@gmail.com>
2026-07-14 08:27:53 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -340,10 +340,10 @@ fn main() -> ExitCode {
             let mut full_str = String::new();
 
             while let Some(token) = token_iter.next(){ // [NUMBER 50 50.0]
-                let ind_tokens: Vec<&str> = token.split(" ").collect()
-                if let Some(tk_type) = ind_token.get(0){ // <String>.split(space) => Vec<&str>, so get(0) = Option<&&str> Some<&tk_type> = Option<&&str>
+                let ind_tokens: Vec<&str> = token.split(" ").collect();
+                if let Some(tk_type) = ind_tokens.get(0){ // <String>.split(space) => Vec<&str>, so get(0) = Option<&&str> Some<&tk_type> = Option<&&str>
                  match tk_type{
-                    "NUMBER" => full_str.push_str("{}", *ind_tokens.get(2).copied().unwrap_or(&"")), // &&str 
+                    "NUMBER" => full_str.push_str("{}", *ind_tokens.get(2).unwrap_or(&"").to_string()), // &&str 
                     "STRING" => {
                         let str_tok: Vec<String> = token.split('"').map(|s| s.to_string()).collect();
                         full_str.push_str("{}", *str_tok[1]); 
@@ -351,12 +351,12 @@ fn main() -> ExitCode {
                     "LEFT_PAREN" => full_str.push_str("group"),
                     "RIGHT_PAREN" => {},
                     _ => {
-                        full_str.push_str!("{}", *ind_tokens.get(1).copied().unwrap_or(&""));
+                        full_str.push_str("{}", *ind_tokens.get(1).unwrap_or(&"").to_string());
                     }
                 };// end match
                 };
             };// end while loop
-            return full_str
+            return full_str;
             return ExitCode::from(0)
         },
         _ => {
