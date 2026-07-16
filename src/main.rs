a3435beeddab80ff8e6707d2b4964473fbe61e2b
Adi Shankar <adishankar219@gmail.com>
2026-07-16 13:30:02 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -123,7 +123,7 @@ impl Parser{
         let curr_tok = self.tokens[self.current].to_string();
         let words: Vec<&str> = curr_tok.split(" ").collect();
         let curr_type = *words.get(0).unwrap_or(&""); 
-        return curr_type.to_string()
+        return &curr_type.to_string()
     }
 }
 
