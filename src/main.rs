4363d2415d247641c10f33a14edbc4c23451fe7e
Adi Shankar <adishankar219@gmail.com>
2026-07-18 10:16:26 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -366,7 +366,7 @@ fn main() -> ExitCode {
                 for j in err{
                    eprintln!("{}", j);
                 };
-                return ExitCode::from(1)
+                return ExitCode::from(65)
             };
             return ExitCode::from(0)
            
