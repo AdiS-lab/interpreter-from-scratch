1edbf3fdc06503c9d7f7f8de6fddcfabf6b99e51
Adi Shankar <adishankar219@gmail.com>
2026-07-31 11:23:08 -0700

    codecrafters submit [skip ci]

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -11,7 +11,7 @@ pub struct Interpreter {
 
 impl Interpreter {
     pub fn evaluate(&mut self, expr: Expr) -> Result<Lit, String> {
-        println!("{:?}", &self.scope);
+        // println!("{:?}", &self.scope);
         match expr {
             Expr::Literal(lit) => {
                 if let Lit::Id(s) = lit {
